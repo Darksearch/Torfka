a5b4077b3fb64a3f854ce57e8f481fe1fa5b988d
Vishal <vlall@users.noreply.github.com>
2016-01-25 02:27:26 -0500

    Update onion-status.py

--- onion-status.py
+++ onion-status.py
@@ -1,29 +1,55 @@
 import urllib2
-import httplib
 from torfka import Torfka
+import requests
+from time import gmtime, strftime
+from urllib2 import Request, urlopen, URLError
 
-# Testing success Codes
-if __name__ == '__main__':
-	torf = Torfka(9050, 'localhost')
-	success = 0
-	failure = 0
-	onionList = 'onions.txt'
-	with open(onionList) as onionList:
-		content = onionList.readlines()
 
-	for url in content:
-	    try:
-	        connection = urllib2.urlopen(url)
-	        print ('%s,%s') % (str(url.rstrip()), str(connection.getcode()))
-	        connection.close()
-	        if connection.getcode() == 200:
-	        	success += 1
-	    except:
-	        print ('%s,%s') % (str(url.rstrip()),'404')
-	        failure += 1
-		fileOut = open("output.csv", "a")
-		fileOut.write(msg + '\n')
+def working(site):
+    req = urllib2.Request(site)
+    try:
+        urllib2.urlopen(req)
+        print('%s, 200') % site
+        return True
 
-	fileOut.close()
-	print ('Successes: %d, Failures: %d' % (success,failure))
-	torf.reset()
+    except Exception:
+        print('%s, 404') % site
+        return False
+
+
+def main():
+    torf = Torfka(9150, 'localhost')
+
+    with open('onionlinks.txt') as sites:
+        for onion in sites:
+            #  Remove CHOMP.
+            onion = onion.rstrip()
+            if working(onion):
+
+                #  Write to HTML
+                domain = onion.split('://')[1]
+                domain = domain.split('.onion')[0]
+                domain = str(domain + '.html')
+                content = urllib2.urlopen(onion).read()
+                Html_file = open('data/%s' % (domain), "w")
+                Html_file.write(content)
+                #  Log
+                clock = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                logContent = (
+                    '%s, %s, %s, %d\n' % (
+                                        str(clock),
+                                        str(onion),
+                                        domain,
+                                        len(content)
+                                    )
+                )
+                print logContent
+                with open("logs/scrape.log", "a") as log:
+                    log.write(logContent)
+                urllib2.urlopen(onion).close()
+                Html_file.close()
+    sites.close()
+    torf.reset()
+
+
+main()
